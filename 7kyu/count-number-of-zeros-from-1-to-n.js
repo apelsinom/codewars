f02659afa7e04384753d5249af21d2e545c83b55
apelsinom <apelsinom@mail.ru>
2023-09-03 13:30:12 +0300

    Count number of zeros from 1 to N

--- 7kyu/count-number-of-zeros-from-1-to-n.js
+++ 7kyu/count-number-of-zeros-from-1-to-n.js
@@ -3,7 +3,6 @@
 // Examples:
 // There are 2 zeros from 1 to 20: 10, 20
 // There are 11 zeros from 1 to 100: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
-//
 // countZeros(10); // returns 1
 // countZeros(100); // returns 11
 // countZeros(200); // returns 31
