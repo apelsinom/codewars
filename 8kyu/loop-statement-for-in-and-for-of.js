a9b1ec24ea9ccb12f7b2c9a0762c044e0b22b690
apelsinom <apelsinom@mail.ru>
2023-09-05 00:56:53 +0300

    Training JS #12: loop statement --for..in and for..of

---
+++ 8kyu/loop-statement-for-in-and-for-of.js
@@ -0,0 +1,21 @@
+// ❓ DESCRIPTION:
+//    The function giveMeFive accepts 1 parameter, obj, which is an object.
+//    Create an array (which you will eventually return). Then, traverse obj,
+//    checking each key and value. If the length of the key is equal to 5,
+//    then push the key to your array. Separately, if the length of the
+//    value is equal to 5, then push the value to your array.
+//    At the end, return your array.
+
+//    You should use for..in in your code, otherwise your solution may not
+//    pass this kata.
+// ✅ SOLUTION:
+function giveMeFive(obj){
+    const objArr = [];
+    for (let key in obj) {
+        if(key.length===5) objArr.push(key)
+        if((obj[key]).length===5) objArr.push(obj[key])
+    }
+    return objArr;
+}
+
+console.log(giveMeFive({Ihave:"enough", money:"to",buy:"a",car:"model"}))
