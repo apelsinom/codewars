e958e61ceb766318608ec4edc198abf6e1d71b21
apelsinom <apelsinom@mail.ru>
2023-09-06 10:05:49 +0300

    Sum of Multiples

--- 8kyu/sum-of-multiples.js
+++ 8kyu/sum-of-multiples.js
@@ -9,3 +9,13 @@
 // Ingredients that are not present in the objects, can be considered as 0.
 
 // ✅ SOLUTION:
+function sumMul(n,m){
+    if(n<=0 || m<=0) return "INVALID";
+    let sum = 0;
+    for (let i=n; i<m; i+=n) {
+        sum +=i;
+    }
+    return sum;
+}
+
+console.log(sumMul(4,-7))
