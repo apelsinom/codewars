b2434c994bb3581b60e963f4a33281485274d1fc
apelsinom <apelsinom@mail.ru>
2023-09-07 18:35:25 +0300

    Filter the number

---
+++ 7kyu/filter-the-number.js
@@ -0,0 +1,16 @@
+// ❓ DESCRIPTION:
+// Oh, no! The number has been mixed up with the text.
+// Your goal is to retrieve the number from the text,
+// can you return the number back to its original state?
+//     Task
+//     Your task is to return a number from a string.
+//     Details
+// You will be given a string of numbers and letters mixed
+// up, you have to return all the numbers in that string in the order they occur.
+
+// ✅ SOLUTION:
+function filterString(value) {
+    return  +value.split('').filter(i => !isNaN(i)).join('');
+}
+
+console.log(filterString("aa1bb2cc3dd"))
